41ed23000c52e67cf20eb7aad6a5c5f362f68431
Arkadeep Ghorai <arkadeepghr@gmail.com>
2026-04-17 13:43:40 +0530

    Added models2

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+#condition model
+class Condition(models.Model):
+    name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
 
 #category model
 class Category(models.Model):
@@ -19,7 +24,8 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
+    
+    condition = models.ForeignKey(Condition, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     semester = models.ForeignKey(Semester, on_delete=models.SET_NULL, null=True)
 
